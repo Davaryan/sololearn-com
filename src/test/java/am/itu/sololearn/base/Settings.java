79b563ac13523d35313d9227947f532579dac26c
Davaryan <ndavaryan@gmail.com>
2021-02-17 20:56:18 +0400

    driver creation is udated

--- src/test/java/am/itu/sololearn/base/Settings.java
+++ src/test/java/am/itu/sololearn/base/Settings.java
@@ -2,7 +2,7 @@ package am.itu.sololearn.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 
@@ -13,14 +13,14 @@ public class Settings {
 	@BeforeClass
 	public void setUp() {
 //		System.setProperty("webdriver.chrome.driver", "C:\\Windows\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get("https://sololearn.com");
 		driver.manage().window().maximize();
-		System.out.println("after wait");
+		
 	}
 	
-//	@AfterClass
-//	public void tearDown() {
-//		driver.quit();
-//	}
+	@AfterClass
+	public void leavePage() {
+		driver.quit();
+	}
 }
